c79f6e0bda6a86210077d067c096962c6fc90c09
Nella <nella611@yandex.ru>
2022-01-25 18:30:43 +0200

    added creater folder

--- page_loader/download.py
+++ page_loader/download.py
@@ -3,10 +3,14 @@ import re
 import requests
 
 
-def get_file_name(page_url):
-    file_name, ext = os.path.splitext(page_url.split('//')[1])
-    file_name = re.sub("[^A-Za-z0-9]", "-", file_name) + '.html'
-    return file_name
+def get_url_without_scheme(url):
+    return url.split('//')[1]
+
+
+def get_correct_name_and_ext(page_url):
+    file_name, ext = os.path.splitext(page_url)
+    file_name = re.sub("[^A-Za-z0-9]", "-", file_name)
+    return file_name, ext
 
 
 def get_data(page_url):
@@ -22,10 +26,15 @@ def write_file(path, data):
 
 
 def download(page_url, download_path):
-    file_name = get_file_name(page_url)
+    url_without_scheme = get_url_without_scheme(page_url)
+    file_name, _ = get_correct_name_and_ext(url_without_scheme)
     if not download_path:
         download_path = os.getcwd()
-    file_path = os.path.join(download_path, file_name)
+    # запись страница в файл
+    file_path = os.path.join(download_path, file_name + '.html')
     data = get_data(page_url)
     write_file(file_path, data)
+    # создание папки для картинок
+    download_folder = os.path.join(download_path, file_name + '_files')
+    os.mkdir(download_folder)
     return file_name
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,15 +1,39 @@
 import os
 import tempfile
+import pytest
 import requests_mock
 from page_loader import download
 
 
 test_dirname = os.path.dirname(__file__)
-expected_file = os.path.join(test_dirname, 'fixtures', 'ru-hexlet-io-courses.html')
+created_html = 'ru-hexlet-io-courses.html'
+created_dir = 'ru-hexlet-io-courses_files'
+created_img = 'ru-hexlet-io-assets-professions-nodejs.png'
+expected_file = os.path.join(test_dirname, 'fixtures', created_html)
+expected_dir = os.path.join(test_dirname, 'fixtures', created_dir)
 page_url = 'https://ru.hexlet.io/courses'
 
 
-def test_download_worked():
+parameters_mark = [
+    created_html,
+    created_dir,
+    created_img,
+]
+
+
+@pytest.mark.parametrize('expected_name', parameters_mark)
+def test_new_file_is_created(expected_name):
+    with open(expected_file) as f:
+        exp_data = f.read()
+        with requests_mock.Mocker() as mock:
+            mock.get(page_url, text=exp_data)
+            with tempfile.TemporaryDirectory() as directory:
+                download(page_url, directory)
+                expected_path = os.path.join(directory, expected_name)
+                assert os.path.exists(expected_path)
+
+
+def test_data_is_download():
     with open(expected_file) as f:
         exp_data = f.read()
         with requests_mock.Mocker() as mock:
@@ -17,6 +41,5 @@ def test_download_worked():
             with tempfile.TemporaryDirectory() as directory:
                 download(page_url, directory)
                 expected_path = os.path.join(directory, 'ru-hexlet-io-courses.html')
-                assert os.path.exists(expected_path)
                 with open(expected_path) as f:
                     assert exp_data == f.read()
