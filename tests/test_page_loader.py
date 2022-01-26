545769a04b952a3f29317e460cc45d97274114ab
Nella <nella611@yandex.ru>
2022-01-26 14:02:03 +0200

    added tests for data

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -8,22 +8,22 @@ from page_loader import download
 test_dirname = os.path.dirname(__file__)
 created_html = 'ru-hexlet-io-courses.html'
 created_dir = 'ru-hexlet-io-courses_files'
-created_img = 'ru-hexlet-io-assets-professions-nodejs.png'
+created_img = os.path.join(created_dir, 'ru-hexlet-io-assets-professions-nodejs.png')
 expected_file = os.path.join(test_dirname, 'fixtures', created_html)
 expected_dir = os.path.join(test_dirname, 'fixtures', created_dir)
-expected_img = os.path.join(expected_dir, created_img)
+expected_img = os.path.join(test_dirname, 'fixtures', created_img)
 page_url = 'https://ru.hexlet.io/courses'
 image_url = 'https://ru.hexlet.io/assets/professions/nodejs.png'
 
 
-parameters_mark = [
+parameters_exists = [
     created_html,
     created_dir,
     created_img,
 ]
 
 
-@pytest.mark.parametrize('expected_name', parameters_mark)
+@pytest.mark.parametrize('expected_name', parameters_exists)
 def test_new_file_is_created(expected_name):
     with open(expected_file) as f:
         exp_data = f.read()
@@ -35,11 +35,10 @@ def test_new_file_is_created(expected_name):
                 with tempfile.TemporaryDirectory() as directory:
                     download(page_url, directory)
                     expected_path = os.path.join(directory, expected_name)
-                    print(expected_path)
                     assert os.path.exists(expected_path)
 
-# написать параметры для фала и картинки
-def test_data_is_download(expected_data):
+
+def test_page_is_download():
     with open(expected_file) as f:
         exp_data = f.read()
         with open(expected_img, 'rb') as image:
@@ -52,3 +51,18 @@ def test_data_is_download(expected_data):
                     expected_path = os.path.join(directory, created_html)
                     with open(expected_path) as f:
                         assert exp_data == f.read()
+
+
+def test_images_is_download():
+    with open(expected_file) as f:
+        exp_data = f.read()
+        with open(expected_img, 'rb') as image:
+            exp_img = image.read()
+            with requests_mock.Mocker() as mock:
+                mock.get(page_url, text=exp_data)
+                mock.get(image_url, content=exp_img)
+                with tempfile.TemporaryDirectory() as directory:
+                    download(page_url, directory)
+                    expected_path = os.path.join(directory, created_img)
+                    with open(expected_path, 'rb') as f:
+                        assert exp_img == f.read()
