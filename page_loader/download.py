ccfc40f39d4a0336d1081991c614e8ca6d31dcc1
Nella <nella611@yandex.ru>
2022-01-27 13:29:38 +0200

    added changed_path

--- page_loader/download.py
+++ page_loader/download.py
@@ -20,8 +20,6 @@ def get_data(page_url):
     return data.text
 
 
-# надо ли проверять, что такой файл создан?
-# что делать, если страница уже загружена?
 def write_file(path, data):
     with open(path, 'w+') as f:
         f.write(data)
@@ -32,38 +30,41 @@ def get_base_url(url):
     return os.path.join(f'{url.scheme}://', url.netloc)
 
 
-def get_images_link(data, base_url):
+def get_images_path(data, base_url):
     soup = BeautifulSoup(data, 'html.parser')
     images = soup.find_all('img')
-    images_links = []
+    images_paths = []
     for image in images:
         src = image.get('src')
         if not src.startswith('http'):
             src = urljoin(base_url, src)
-            images_links.append(src)
-    return images_links
+            images_paths.append(src)
+    return images_paths
 
 
-def download_images(images_links, dir_path):
-    for link in images_links:
-        images_name = get_url_without_scheme(link)
+def download_images(images_paths, dir_path):
+    new_path = []
+    for path in images_paths:
+        images_name = get_url_without_scheme(path)
         images_name, exp = get_correct_name_and_ext(images_name)
         images_name = images_name + exp
         image_path = os.path.join(dir_path, images_name)
-        image = requests.Session().get(link)
+        image = requests.Session().get(path)
         with open(image_path, 'wb') as f:
             f.write(image.content)
+        new_path.append(image_path)
+    return new_path
 
 
-# нужен список новых линков
-# объединить с парсером?
-def changed_links(file, new_path):
+def changed_path(file, new_path, download_path):
     with open(file, 'r') as fp:
         f = fp.read()
     soup = BeautifulSoup(f, features="html.parser")
     for image in soup.find_all('img'):
-        for link in new_path:
-            image['src'] = link
+        for path in new_path:
+            print(download_path)
+            print(path)
+            image['src'] = os.path.relpath(path, download_path)
     soup = soup.prettify()
     with open(file, 'w') as f:
         f.write(soup)
@@ -84,8 +85,8 @@ def download(page_url, download_path):
     # узнаем базовую url
     base_url = get_base_url(page_url)
     # записываем файлы в папку
-    images_link = get_images_link(data, base_url)
-    download_images(images_link, download_folder)
+    images_path = get_images_path(data, base_url)
+    new_image_path = download_images(images_path, download_folder)
     # заменяем ссылки в новом html
-    
+    changed_path(file_path, new_image_path, download_path)
     return file_name
