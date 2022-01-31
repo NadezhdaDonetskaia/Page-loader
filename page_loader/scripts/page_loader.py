a080d4f88202338d4866df1a41739d2e342f20d5
Nella <nella611@yandex.ru>
2022-01-31 16:38:47 +0200

    changed info app

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -2,19 +2,19 @@
 import argparse
 from page_loader import download
 
-parser = argparse.ArgumentParser(description='Page loader,',
-                                 prog='page-loader')
-parser.add_argument('page_url', metavar='page_url',
-                    help='page url for download')
-parser.add_argument('-f', '--output', dest='OUTPUT',
-                    default='',
-                    help='enter download directory')
+parser = argparse.ArgumentParser(description='Page loader',
+                                 prog='page-loader', usage='%(prog)s [options] <url>')
+parser.add_argument('page_url', metavar='<url>',
+                    help='enter page address'
+                    )
+parser.add_argument('-o', '--output',
+                    help='output dir (default: current folder)')
 
 args = parser.parse_args()
 
 
 def main():
-    print(download(args.page_url, args.OUTPUT))
+    print(download(args.page_url, args.output))
 
 
 if __name__ == '__main__':
